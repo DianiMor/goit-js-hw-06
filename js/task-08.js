3ad889ae3e00fa3452ffd3e0e71682f45ba6db49
DianiMor <diana.3mol@gmail.com>
2023-12-11 19:13:42 -0500

    java6act3

--- js/task-08.js
+++ js/task-08.js
@@ -1,21 +1,21 @@
 const formulario = document.querySelector(".login-form");
+function submitForm(event) {
+  event.preventDefault();
 
-formulario.addEventListener("submit", submitForm);
-event.preventDefault();
+  const form = Event.target;
 
-const form = event.target; // Corrección: Cambiar 'form' por 'event.target'
+  const mail = form.elements.email;
+  const passw = form.elements.password;
 
-const mail = form.elements.email;
-const passw = form.elements.password;
+  if (email.value === "" || passw.value === "") {
+    alert("Todos los espacios deben ser rellenados");
+  } else {
+    const user = {
+      email: mail.value,
+      password: passw.value,
+    };
 
-if (email.value === "" || passw.value === "") {
-  alert("Todos los espacios deben ser rellenados");
-} else {
-  const user = {
-    email: mail.value,
-    password: passw.value,
-  };
-
-  console.log(user);
-  formulario.reset();
+    console.log(user);
+    formulario.reset();
+  }
 }
